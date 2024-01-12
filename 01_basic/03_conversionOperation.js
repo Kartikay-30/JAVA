efe987e2b9cfa2c748ed77f4e309723589f0a011
Kartikay <155715317+Kartikay-30@users.noreply.github.com>
2024-01-12 09:21:12 +0000

    container config done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Kartik"
+
+// converstion number to boolean
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Kartik" => true
+
+let someNumber = "33"
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
